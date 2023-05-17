f452b2fb3c6a2d17cdbbeaad70cd663dbc4ec470
anegrizotti <algrizotti@gmail.com>
2023-05-17 09:39:43 -0300

    feat: configuração para renderizar a imagem adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,66 +15,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        backgroundColor: Color(0xFFFFF197),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
+      home: Builder(
+        builder: (context) => Scaffold(
+          backgroundColor: Color(0xFFFFF197),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  'MATH',
-                  style: TextStyle(
-                    color: Color(0xFFEBA1CE),
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Color(0xFFC7EBF2),
+                Column(
+                  children: [
+                    Text(
+                      'MATH',
+                      style: TextStyle(
+                        color: Color(0xFFEBA1CE),
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Color(0xFFC7EBF2),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Text(
+                      'COLOR',
+                      style: TextStyle(
+                        color: Color(0xFFEBA1CE),
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Color(0xFFC7EBF2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  'COLOR',
-                  style: TextStyle(
-                    color: Color(0xFFEBA1CE),
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Color(0xFFC7EBF2),
-                      ),
-                    ],
+                SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SelectSubjectScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFB6D993),
+                    onPrimary: Colors.black,
                   ),
+                  child: const Text('Iniciar', style: TextStyle(fontSize: 30)),
+                ),
+                SizedBox(height: 50),
+                Image.asset(
+                  path.join('lib', 'assets', 'telainicial.png'),
+                  width: 200,
                 ),
               ],
             ),
-            SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SelectSubjectScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFFB6D993), // sets the background color
-                onPrimary: Colors.black, // sets the text color
-              ),
-              child: const Text('Iniciar', style: TextStyle(fontSize: 30)),
-            ),
-            SizedBox(height: 50),
-            Image.asset(
-              'assets/telainicial.png', // o caminho da imagem dentro do diretório lib
-              width: 200, // ajusta a largura da imagem
-            ),
-          ],
+          ),
         ),
       ),
     );
