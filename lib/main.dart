38ccf185a456258e0125f528613c867bfd0b98fc
Rafaela <rafalasantosdarosa@yahoo.com>
2023-05-17 11:02:33 -0300

    alterações tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:math_color/select_subject_screen.dart';
-// ignore: depend_on_referenced_packages
 import 'package:path/path.dart' as path;
 
 void main() {
@@ -18,74 +17,97 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Builder(
-        builder: (context) => Scaffold(
-          backgroundColor: Color(0xFFFFF197),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
+        builder: (context) {
+          final double screenHeight = MediaQuery.of(context).size.height;
+          final double screenWidth = MediaQuery.of(context).size.width;
+
+          return Scaffold(
+            backgroundColor: Color(0xFFFFF197),
+            body: SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      'MATH',
-                      style: TextStyle(
-                        color: Color(0xFFEBA1CE),
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        shadows: [
-                          Shadow(
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 3.0,
-                            color: Color(0xFFC7EBF2),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'MATH',
+                            style: TextStyle(
+                              color: Color(0xFFEBA1CE),
+                              fontSize: screenHeight * 0.1,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'bungee',
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(2.0, 2.0),
+                                  blurRadius: 5.0,
+                                  color: Color(0xFF00FFFF),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            'COLOR',
+                            style: TextStyle(
+                              color: Color(0xFFEBA1CE),
+                              fontSize: screenHeight * 0.1,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'bungee',
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(2.0, 2.0),
+                                  blurRadius: 5.0,
+                                  color: Color(0xFF00FFFF),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    Text(
-                      'COLOR',
-                      style: TextStyle(
-                        color: Color(0xFFEBA1CE),
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        shadows: [
-                          Shadow(
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 3.0,
-                            color: Color(0xFFC7EBF2),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: screenHeight * 0.06),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SelectSubjectScreen()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFFB6D993),
+                          onPrimary: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(screenHeight * 0.04),
+                            side: BorderSide(color: Colors.black, width: 2),
                           ),
-                        ],
+                          minimumSize:
+                              Size(screenWidth * 0.5, screenHeight * 0.08),
+                        ),
+                        child: Text('INICIAR',
+                            style: TextStyle(fontSize: screenHeight * 0.05)),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    Expanded(
+                      child: Image.asset(
+                        path.join('lib', 'assets', 'telainicial.png'),
+                        fit: BoxFit.contain,
+                        height: screenHeight * 0.3,
                       ),
                     ),
                   ],
                 ),
-                SizedBox(height: 50),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SelectSubjectScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFFB6D993),
-                    onPrimary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                  child: const Text('INICIAR', style: TextStyle(fontSize: 30)),
-                ),
-
-                SizedBox(height: 50),
-                Image.asset(
-                  path.join('lib', 'assets', 'telainicial.png'),
-                  width: 200,
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
